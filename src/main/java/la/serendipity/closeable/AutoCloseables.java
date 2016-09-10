8c430a57887818f81c237f0deb0d90b685fa65c6
Kazuki MATSUDA <matsuda@ozaki.holdings>
2016-09-11 00:42:08 +0900

    Add AutoCloseableTest.

--- src/main/java/la/serendipity/closeable/AutoCloseables.java
+++ src/main/java/la/serendipity/closeable/AutoCloseables.java
@@ -3,7 +3,9 @@ package la.serendipity.closeable;
 import java.util.concurrent.ExecutorService;
 
 import lombok.NonNull;
+import lombok.experimental.UtilityClass;
 
+@UtilityClass
 public class AutoCloseables {
     public static CloseableExecutorService closeable(@NonNull final ExecutorService executorService) {
         return new CloseableExecutorServiceImpl(executorService);
